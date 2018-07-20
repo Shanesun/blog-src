0a9fbb785350dfe43e2b1271907946281a7c7f68
boris1993 <boris1993@126.com>
2018-07-20 11:00:51 +0800

    Add lib Pangu

--- update-libs.sh
+++ update-libs.sh
@@ -3,4 +3,5 @@
 git subtree pull --prefix=themes/next next master
 git subtree pull --prefix=themes/next/source/lib/fancybox fancybox master
 git subtree pull --prefix=themes/next/source/lib/pace pace master
+git subtree pull --prefix=themes/next/source/lib/pangu pangu master
 
